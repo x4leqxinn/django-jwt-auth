34e0c2aab603430692f9a13c0589705e59e063ae
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-22 00:06:54 -0400

    ADD: Celery setup

---
+++ core/core/celery.py
@@ -0,0 +1,17 @@
+#Activamos los imports absolutos para evitar conflictos entre packages
+from __future__ import absolute_import
+
+import os
+from celery import Celery
+
+# Establecer la configuración de Django para Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Crea una instancia de la aplicación Celery
+app = Celery('core')
+
+# Lee la configuración de Celery desde la configuración de Django
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Descubre y registra automáticamente las tareas en tu aplicación Django
+app.autodiscover_tasks()
