30b4001c9ae485efd7a950a12447a7e26e33e66b
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-23 18:56:56 -0400

    ADD: CustomTokenObtainPairView

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
 
-# Create your views here.
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+
+
+
