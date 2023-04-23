7ee35f1b204e5831196fb98602aa1dfb8d379cba
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-23 19:39:01 -0400

    DELETE: Old structure project

--- apps/base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-urlpatterns = [
-    path('schema/',SpectacularAPIView.as_view(api_version="v1"),name='schema'),
-    path('schema/swagger/',SpectacularSwaggerView.as_view(url_name='api-base:schema'),name='swagger'),
-]
