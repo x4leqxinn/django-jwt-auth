de3b0bd27ed17d92fe62589e49027b84fbf20c47
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-23 04:40:41 -0400

    ADD: Send mail temp

--- task/views.py
+++ task/views.py
@@ -6,7 +6,35 @@ from rest_framework import viewsets
 from .serializers import TaskSerializer
 from .models import Task
 
-from .tasks import add
+#from .tasks import add
+
+
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+def send_mail():
+    # Procesar los datos que desea enviar por correo electrónico
+    user = {
+        'name' : 'Jorge'
+    }
+    destination = "xxxxx@gmail.com"
+
+    # Renderizar un template HTML con los datos procesados
+    context = {'user': user}
+    message = render_to_string('mails/account/locked.html', context)
+
+    # Crear un objeto EmailMessage y enviar el correo electrónico
+    mail = EmailMessage(
+        '{} Tu cuenta ha sido bloqueda.'.format(user.get('name')),
+        message,
+        'xxxxx@gmail.com',
+        [destination],
+        reply_to=['xxxxxx@gmail.com']
+    )
+    mail.content_subtype = "html"  # Para enviar el correo con formato HTML
+    mail.send()
+
 
 @extend_schema_view(
     list=extend_schema(description='Permite obtener una lista de tareas.'),
@@ -25,7 +53,16 @@ class TaskViewSet(viewsets.ModelViewSet):
         response = super().create(request, *args, **kwargs)
 
         # Agrega código adicional aquí para extender la funcionalidad del método post
-        resultado = add.delay(10, 10)
-        print(resultado)
+        #resultado = add.delay(10, 10)
+        #print(resultado)
         # Retorna la respuesta original del método create
+
+        # TEST DE ENVIO DE CORREOS
+        send_mail()
+
         return response
+
+
+
+
+
