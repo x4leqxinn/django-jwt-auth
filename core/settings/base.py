36ca6e63b4edaadcdb2dcdc071f7cff0ec83316c
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-22 22:51:06 -0400

    UPDATE: Language code ES

--- core/settings/base.py
+++ core/settings/base.py
@@ -113,14 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
-USE_TZ = True
-
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
