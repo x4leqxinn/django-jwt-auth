4e2966a67105729452203b3b46374a9666ff3df8
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-24 01:01:04 -0400

    ADD: Account locked logic

--- api/v1/authentication/serializers.py
+++ api/v1/authentication/serializers.py
@@ -9,37 +9,44 @@ from django.utils import timezone
 
 class CustomTokenObtainSerializer(TokenObtainSerializer):
 
+    profile = None
     default_error_messages = {
         **TokenObtainPairSerializer.default_error_messages,
         'failed_attempts' : 'Has ingresado credenciales incorrectas 3 veces. Tu cuenta ha sido bloqueada por 5 minutos.',
     }
+    
+    
+
+    def lock_account(self):
+        self.profile.lock_account(5)
+        raise exceptions.AuthenticationFailed(
+            self.error_messages['failed_attempts'],
+                'failed_attempts'
+        )
+
 
     def verify_attempts(self,attrs):
         current_user = User.objects.filter(email=attrs.get(self.username_field)).first()
         if not current_user: return
+
         # Si las credenciales son incorrectas, incrementamos el contador de intentos fallidos
-        profile,_ = UserProfile.objects.get_or_create(user=current_user)
-        profile.increment_failed_attempts()
-        if profile.failed_attempts >= 3:
-            # Si el usuario ha fallado 3 veces, bloqueamos su cuenta por 5 minutos
-            profile.lock_account(5)
-            raise exceptions.AuthenticationFailed(
-                self.error_messages['failed_attempts'],
-                'failed_attempts'
-            )
+        self.profile,_ = UserProfile.objects.get_or_create(user=current_user)
+        self.profile.increment_failed_attempts()
+        
+        if self.profile.failed_attempts >= 3: self.lock_account()
 
     def verify_account_blocking(self):
-        profile,_ = UserProfile.objects.get_or_create(user=self.user)
+        self.profile,_ = UserProfile.objects.get_or_create(user=self.user)
         
-        if profile.is_account_locked():
-            remaining_time = (profile.locked_until - timezone.now()).seconds // 60
+        if self.profile.is_account_locked():
+            remaining_time = (self.profile.locked_until - timezone.now()).seconds // 60
             self.error_messages['account_locked'] = f'Tu cuenta está bloqueada, vuelve a intentarlo dentro de {remaining_time} minutos.'
             raise exceptions.AuthenticationFailed(
                 self.error_messages['account_locked'],
                 'account_locked'
             )
         
-        profile.reset_failed_attempts() 
+        self.profile.reset_failed_attempts() 
 
     def validate(self, attrs: Dict[str, Any]) -> Dict[Any, Any]:
         authenticate_kwargs = {
