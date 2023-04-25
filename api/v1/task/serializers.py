7605e0461190686d17a7fc48d0748a073344c221
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-25 02:54:16 -0400

    DELETE: APP Tasks

--- api/v1/task/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Task
-from rest_framework import serializers
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
