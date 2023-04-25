71ca4a34af9a07dc630be8a7c4a624447125f211
Jorge Quintui <jorgealequinn@gmail.com>
2023-04-25 02:56:18 -0400

    DELETE: Old task url

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('documentation/', include(('api.v1.base.urls','api-base'),namespace='api-base')),
-    path('task/', include(('api.v1.task.urls', 'api-tasks'), namespace='api-tasks')),
+    path('docs/', include(('api.v1.base.urls','api-base'),namespace='api-base')),
     path('auth/', include(('api.v1.authentication.urls','api-auth'),namespace='api-auth')),
 ]
