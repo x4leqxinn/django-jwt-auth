3428b8b3f453dd4c8f3b1b2734349ae3af44b5ce
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-17 00:27:05 -0400

    FIX: Delete email host

--- core/settings/development.py
+++ core/settings/development.py
@@ -18,12 +18,3 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-
-
-# Email backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
