ea871521cb6dccfe99149d1be80378f26d370122
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-18 20:46:13 -0400

    ADD: Products models

--- api/v1/base/models.py
+++ api/v1/base/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class BaseModel(models.Model):
     id = models.AutoField(primary_key=True)
-    state = models.CharField(max_length=15, null=False, blank=False, default='ACTIVO')
     created_at = models.DateTimeField('Fecha de Creación',auto_now=False, auto_now_add=True, null=False)
     updated_at = models.DateTimeField('Fecha de Actualización', auto_now=True, auto_now_add=False, null=False)
 
--- api/v1/product/models.py
+++ api/v1/product/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from api.v1.base.models import BaseModel
 
 # Create your models here.
+
+class ProductCategory(BaseModel):
+    description = models.CharField(max_length=100,null=False,blank=False,verbose_name='Descripción de la categoría')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = 'Categoría de producto'
+        verbose_name_plural = 'Categorías de productos'
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100,null=False,blank=False,verbose_name='Nombre del producto')
+    description = models.CharField(max_length=100,null=False,blank=False,verbose_name='Descripción del producto')
+    price = models.IntegerField(verbose_name='Precio del producto')
+    stock = models.IntegerField(verbose_name='Stock')
+    category = models.ForeignKey(ProductCategory, on_delete = models.CASCADE, verbose_name = 'Categoría')
+    image = models.ImageField('Imagen ', upload_to='products/', default='', max_length=255, null=True, blank = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
