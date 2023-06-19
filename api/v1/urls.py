1ac046f95d4e446e76d812553b35329e601d7086
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-18 21:10:31 -0400

    ADD: Set product url

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('docs/', include(('api.v1.base.urls', 'api-v1:api-base'), namespace='api-base')),
     path('auth/', include(('api.v1.authentication.urls', 'api-v1:api-auth'), namespace='api-auth')),
+    path('products/',include(('api.v1.product.urls', 'api-v1:api-product'), namespace='api-product')),
 ]
